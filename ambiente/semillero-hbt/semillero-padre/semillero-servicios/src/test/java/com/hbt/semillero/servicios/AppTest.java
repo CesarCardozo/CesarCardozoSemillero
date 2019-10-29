97021982c34e6896272303a6ad94265aea41a49d
CesarCardozo <srfeudalwow@gmail.com>
2019-10-28 21:43:52 -0500

    Update AppTest.java

--- ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/AppTest.java
+++ ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/AppTest.java
@@ -36,5 +36,13 @@ public class AppTest {
 		
 		cadenaInvertidaEsperada = "praebu";//cambio de la cadena esperada invertida a un valor equivocado
 		Assert.assertNotEquals(AppTest.invertirCadena(cadena), cadenaInvertidaEsperada); //Prueba con cadena esperada invertida equivocada 
+		
+		cadena = "";//cambio de la cadena esperada invertida a un valor vacio
+		cadenaInvertidaEsperada = "";//cambio de la cadena esperada invertida a un valor acertado
+		Assert.assertEquals(AppTest.invertirCadena(cadena), cadenaInvertidaEsperada); //Prueba con cadena esperada invertida acertada 
+		
+		cadena = "";//cambio de la cadena esperada invertida a un valor vacio
+		cadenaInvertidaEsperada = " ";//cambio de la cadena esperada invertida a un valor equivocado
+		Assert.assertNotEquals(AppTest.invertirCadena(cadena), cadenaInvertidaEsperada); //Prueba con cadena esperada invertida equivocada 
 	}
 }

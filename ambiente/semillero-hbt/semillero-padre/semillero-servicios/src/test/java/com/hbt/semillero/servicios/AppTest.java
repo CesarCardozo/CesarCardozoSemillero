8aac8ac82c9215bdd1439577b93034f8479f397f
CesarCardozo <srfeudalwow@gmail.com>
2019-10-29 22:02:25 -0500

    Ejercicio Sesion 2 java

--- ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/AppTest.java
+++ ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/AppTest.java
@@ -3,6 +3,8 @@ package com.hbt.semillero.servicios;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.hbt.semillero.entidades.EstadoEnum;
+
 public class AppTest {
 
 	public void shouldAnswerWithTrue() {
@@ -44,4 +46,34 @@ public class AppTest {
 		cadenaInvertidaEsperada = " ";//cambio de la cadena esperada invertida a un valor equivocado
 		Assert.assertNotEquals(AppTest.invertirCadena(cadena), cadenaInvertidaEsperada); //Prueba con cadena esperada invertida equivocada 
 	}
+	
+	/**
+	 * 
+	 * Metodo encargado de realizar las pruebas concernientes al enumerado EstadoEnum
+	 * <b>Caso de Uso</b>
+	 * @author Cesar
+	 *
+	 */
+	@Test
+    public void pruebaEstadoEnum() {
+        EstadoEnum estadoActivo = EstadoEnum.ACTIVO;//Instanciar un enum de la clase EstadoEnum (estado activo)
+        EstadoEnum estadoInactivo = EstadoEnum.INACTIVO;//Otra instancia del estado inactivo (para fines de pruebas)
+        String estadoActivoNombre = estadoActivo.name(); //nombre del estado activo
+        String estadoInactivoNombre = estadoInactivo.name();//nombr del estado inactivo
+        int posEstadoActivo = estadoActivo.ordinal();//posicion del estado activo
+        int posEstadoInactivo = estadoInactivo.ordinal();//posicion de estado inactivo
+        int posicionEsperadaEstodoActivo = 0;//posicion esperada del estado activo
+        int posicionEsperadaEstodoInactivo = 1;//posicion esperada del estado inactivo
+        //Prueba en la que se comprueba que sean iguales las posiciones actuales y esperada del estado activo
+        Assert.assertEquals(posEstadoActivo, posicionEsperadaEstodoActivo);
+        //Prueba en la que se comprueba que sean iguales las posiciones actuales y esperada del estado inactivo
+        Assert.assertEquals(posEstadoInactivo, posicionEsperadaEstodoInactivo);
+        //Array que contiene todos los enum
+        EstadoEnum[] estados = EstadoEnum.values();
+    }
+	
+	//TODO
+    /**
+     * Pediente hacer un método que use el método ToString de la entidad COMIC
+     */
 }

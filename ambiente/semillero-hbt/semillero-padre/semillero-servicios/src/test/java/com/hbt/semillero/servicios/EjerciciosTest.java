195bc572db837b32d953d7cf7d805ffb26b14b18
CesarCardozo <srfeudalwow@gmail.com>
2019-11-13 18:59:32 -0500

    ejercicio 2

--- ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/EjerciciosTest.java
+++ ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/EjerciciosTest.java
@@ -14,7 +14,7 @@ import org.testng.annotations.Test;
  */
 public class EjerciciosTest {
 
-	@Test
+	@Test(enabled=false)
 	public void identificadoresTest() {
 		//valido
 		int A$B = 0;
@@ -22,14 +22,32 @@ public class EjerciciosTest {
         //valido
         String _helloWorld = "holamundo";
         Assert.assertEquals(_helloWorld, "holamundo");
-  		//no valido
-  		String true;
-  		//no valido
-  		String java.lang;
+  		//no valido (comentareado para evitar error)
+//  		String true;
+  		//no valido (comentareado para evitar error)
+//  		String java.lang;
   		//valido
   		String pPublic = "pPublic";
         Assert.assertEquals(_helloWorld, "pPublic");
-  		//no valido
-  		String 1980S;
+  		//no valido (comentareado para evitar error)
+//  		String 1980S;
+	}
+	
+	
+	private String brand;
+	private boolean empty;
+	/**
+	 * 
+	 * Metodo encargado de probar el ejercicio 2
+	 * <b>Caso de Uso</b>
+	 * @author Cesar
+	 *
+	 */
+	@Test
+	public void ejercicioDos() {
+		WaterBottle wb = new WaterBottle();
+		System.out.println("Empty=" + wb.empty);
+		System.out.println(", Brand" + wb.brand);
+		//saldra un error de compilacion pues la clase WaterBottle no existe
 	}
 }
